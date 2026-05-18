5e35311a9fdcb96fb38d04d2633679310b5d432e
erick1-618 <erickcefetbcc@gmail.com>
2026-05-18 20:32:23 -0300

    refactor: change routine to script

---
+++ test_routine.py
@@ -0,0 +1,96 @@
+from random import seed
+from graphs.graphs import dijkstra, generate_random_graph
+from graphs.duan_et_al import sssp_duan_et_al
+import csv
+
+# Garantir reprodutibilidade
+seed(42)
+
+# Quantidade de grafos por configuração
+num_graphs = 5
+
+# Quantidade de vértices
+num_vertices = (5000, 10000, 15000)
+
+# Grau médio
+grau_médio = (1.5, 1.25, 1) 
+
+# Algoritmos
+algorithms = (dijkstra, sssp_duan_et_al)
+
+from time import perf_counter
+import csv
+
+results = []
+
+qt_executions = (
+    len(num_vertices)
+    * len(grau_médio)
+    * len(algorithms)
+    * num_graphs
+)
+
+current_execution = 0
+
+for num_v in num_vertices:
+    for k in grau_médio:
+
+        # conversão para probabilidade equivalente no G(n, p)
+        edge_prob = k / (num_v - 1)
+
+        # estimativa realista de número de arestas
+        edge_estimate = (num_v * k)
+
+        for i in range(num_graphs):
+
+            g = generate_random_graph(num_v, edge_prob)
+
+            for alg in algorithms:
+
+
+                    current_execution += 1
+                    progress = (current_execution / qt_executions) * 100
+
+                    print(
+                        f"\rExecuted {progress:.2f}% "
+                        f"| num_vertices: {num_v} "
+                        f"| k(avg_degree): {k:.2f} "
+                        f"| algorithm: {alg.__name__} "
+                        f"| edge_estimate: {edge_estimate:.0f}      ",
+                        end="",
+                        flush=True
+                    )
+
+                    start_time = perf_counter()
+                    r = alg(g, 0)
+                    end_time = perf_counter()
+
+                    delta_time = end_time - start_time
+
+                    results.append({
+                        "num_vertices": num_v,
+                        "avg_degree_k": k,
+                        "algorithm": alg.__name__,
+                        "execution_time": delta_time,
+                        "edge_estimate": edge_estimate,
+                        "graph": f"n{num_v}k{k:.2f}i{i}"
+                    })
+
+print("\nExecution finished.")
+
+# Salvar os resultados em CSV
+with open('data/first_results.csv', 'w', newline='') as f:
+    writer = csv.DictWriter(
+        f,
+        fieldnames=[
+            "num_vertices",
+            "avg_degree_k",
+            "algorithm",
+            "execution_time",
+            "edge_estimate",
+            "graph"
+        ]
+    )
+
+    writer.writeheader()
+    writer.writerows(results)
